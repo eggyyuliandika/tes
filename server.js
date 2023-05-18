b9e56a6621d00077e5d935f3de1bbdcefeff5dbc
eggyyuliandika <eggyyuliandika@gmail.com>
2023-05-18 14:01:57 +0800

    @fastify/swagger

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const fastify = require("fastify")({ logger: true });
+fastify.register(require("@fastify/swagger"), {
+  exposeRoute: true,
+  routePrefix: "/documentation",
+  swagger: {
+    info: { title: "fastify-api" },
+  },
+});
 fastify.register(require("./routes/items"));
-const PORT = 3000;
+const PORT = 5000;
 
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen({ port: 5000 });
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
