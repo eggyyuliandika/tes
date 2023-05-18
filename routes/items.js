babc76afc61db2c19d97b1630f52eee941ed11e5
eggyyuliandika <eggyyuliandika@gmail.com>
2023-05-18 12:11:18 +0800

    options, validation schema, and handlers

--- routes/items.js
+++ routes/items.js
@@ -1,16 +1,47 @@
 const items = require("../items");
-function itemRoutes(fastify, option, done) {
-    //get all items
-  fastify.get("/items", (req, reply) => {
-    reply.send(items);
-  });
 
-  //get single items
-  fastify.get("/items/:id", (req, reply) => {
+//item schema
+const Item = {
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    name: { type: "string" },
+  },
+};
+
+// options for get all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item,
+      },
+    },
+  },
+  handler: function (req, reply) {
+    reply.send(items);
+  },
+};
+//options for get single items
+const getItemOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: function (req, reply) {
     const { id } = req.params;
     const item = items.find((item) => item.id === id);
     reply.send(item);
-  });
+  },
+};
+function itemRoutes(fastify, option, done) {
+  //get all items
+  fastify.get("/items", getItemsOpts);
+
+  //get single items
+  fastify.get("/items/:id", getItemOpts);
 
   done();
 }
